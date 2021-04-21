e45cdf2b52cac775d47cbce4b51435dfd7994c1a
ByteSizedMe <75525893+ByteSizedMe@users.noreply.github.com>
2021-04-21 17:50:36 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    fill("white");
+    textSize(15);
+    text("RABVILEA",100,50);
+    fill("lightblue");
+    text("INT: Alaways changing,not constant",100,70);
+
+    fill("white");
+    textSize(15);
+    text("UFNITNOC",700,150);
+    fill("lightblue");
+    text("INT: Performs a particular task",700,170);
+
+    fill("white");
+    textSize(15);
+    text("BASDATAES",100,250);
+    fill("lightblue");
+    text("INT: Stores all information",100,270);
+    
+
+    
+    
+}
